14fdc42e5ce1270d6e601904470848e3af7348a3
imaslovv <ilya.maslov.93@list.ru>
2024-06-10 22:04:12 +0300

    fix and delete changeset schedule

--- src/main/resources/db/changelog/v-1.0/14_filling_table_schedule.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset maslov_ilya:1.0.14
---comment schedule
-INSERT INTO schedule (date_time, day_of_week, teacher_id)
-VALUES ('2024-05-20 19:00:00', 'Monday', 1),
-       ('2024-05-22 19:00:00', 'Wednesday', 1),
-       ('2024-05-27 19:00:00', 'Monday', 1),
-       ('2024-05-29 19:00:00', 'Wednesday', 1);
